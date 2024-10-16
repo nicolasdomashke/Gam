4f7c8ec3569ee55aafaab6b122a76e37a500d85d
nicolasdomashke <nicolasdomashke@gmail.com>
2024-10-16 23:24:23 +0300

    Init commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+    private SpriteRenderer sprite;
+    [SerializeField] private float speedMultiplier = 2f;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float xSpeed = Input.GetAxisRaw("Horizontal");
+        float ySpeed = Input.GetAxisRaw("Vertical");
+
+        rigidBody.velocity = new Vector2(xSpeed, ySpeed) * speedMultiplier;
+        animator.SetFloat("speed", rigidBody.velocity.magnitude);
+        if (xSpeed < -.01f)
+        {
+            sprite.flipX = true;
+        }
+        else if (xSpeed > .01f)
+        {
+            sprite.flipX = false;
+        }
+    }
+}
---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Newtonsoft.Json.Linq;
+
+public class QuestManager : MonoBehaviour
+{
+    public GameObject questPrefab; 
+    public GameObject questUI;
+    public Parameters parameters;
+
+    public string startQuestAdress = "./Assets/questlines/start.json";
+    public Dictionary<int, GameObject> activeQuests = new Dictionary<int, GameObject>();
+    public Dictionary<int, HashSet<string>> inactiveQuestsForOtherscene = new Dictionary<int, HashSet<string>>();
+    [Serializable]
+    public class QuestInfo
+    {
+        public string title;
+        public int id;
+        public string tale;
+        public Dictionary<string,int> modifiers = new Dictionary<string,int>();
+        public int scene;
+        public float[] spawnPos = new float[3];
+        public List<string> nextQuestsAdress = new List<string>();
+    }
+    public static QuestInfo MyCreateFromSystemJSON(string jsonString)
+    {
+        QuestInfo questData = JsonConvert.DeserializeObject<QuestInfo>(jsonString);
+        return questData;
+    }
+    public void CreateNewQuest(string addr)
+    {
+        string jsonString = File.ReadAllText(addr);
+        QuestInfo questData = MyCreateFromSystemJSON(jsonString);
+        if(questData.scene!= SceneManager.GetActiveScene().buildIndex)
+        {
+            if (!inactiveQuestsForOtherscene.ContainsKey(questData.scene))
+            {
+                inactiveQuestsForOtherscene.Add(questData.scene, new HashSet<string>());
+            }
+            inactiveQuestsForOtherscene[questData.scene].Add(addr);
+            return;
+        }
+
+        if (!activeQuests.ContainsKey(questData.id))
+        {
+            GameObject newQuest = Instantiate(questPrefab, new Vector3(questData.spawnPos[0], questData.spawnPos[1], questData.spawnPos[2]), new Quaternion(0, 0, 0, 0));
+            //newQuest.transform.position = questData.spawnPos;
+            Debug.Log("Created quest: " + questData.title);
+            QuestHandler newQuestHandler = newQuest.gameObject.GetComponentInChildren<QuestHandler>();
+
+            newQuestHandler.title = questData.title;
+            newQuestHandler.id = questData.id;
+            newQuestHandler.tale = questData.tale;
+            newQuestHandler.modifiers = questData.modifiers;
+            newQuestHandler.nextQuestsAdress = questData.nextQuestsAdress;
+
+            newQuestHandler.PlayerParameters = parameters;
+            newQuestHandler.manager = this;
+
+            activeQuests.Add(questData.id, newQuest);
+        }
+    }
+
+    void Start()
+    {
+        string jsonString = File.ReadAllText(startQuestAdress);
+        QuestInfo startQuestData = MyCreateFromSystemJSON(jsonString);
+        foreach (var addr in startQuestData.nextQuestsAdress)
+        {
+            CreateNewQuest(addr);
+        }
+        if (inactiveQuestsForOtherscene.ContainsKey(SceneManager.GetActiveScene().buildIndex))
+        {
+            foreach (var addr in inactiveQuestsForOtherscene[SceneManager.GetActiveScene().buildIndex])
+            {
+                CreateNewQuest(addr);
+            }
+        }
+    }
+}
